c416d340909adff3af5b45c12e883cede1c57e12
Quangvu256 <trongquangvu2004@gmail.com>
2026-05-06 23:50:29 +0700

    v5

--- backend.py
+++ backend.py
@@ -18,7 +18,7 @@ from pathlib import Path
 
 from sentence_transformers import CrossEncoder
 from langchain_community.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from dotenv import load_dotenv
